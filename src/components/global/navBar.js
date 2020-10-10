40fed090d6f704e8526cc0ab888bfb3159755ef4
Daniel Brierton <danieljbrierton@gmail.com>
2020-10-10 08:12:51 -0400

    settings modal working, needs content

---
+++ src/components/global/navBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './navBar.css'
+
+const NavBar = (props) => {
+
+    return(
+        <nav>
+            <button className="navButton">Custom Page 1</button>
+            <button className="navButton">Custom Page 2</button>
+            <button className="navButton">Custom Page 3</button>
+        </nav>
+    )
+}
+
+export default NavBar
